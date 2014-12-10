02b4c1ab2c9c0a1e6488fc049dfc56f9202d8ac7
Anton Povarov <antoxa@corp.badoo.com>
2014-12-10 17:36:52 +0300

    fixed unrecognized tests

--- test/unrecognized/oldnew_test.go
+++ test/unrecognized/oldnew_test.go
@@ -128,6 +128,8 @@ func TestOldNewOldNew(t *testing.T) {
 func TestOldUToU(t *testing.T) {
 	popr := math_rand.New(math_rand.NewSource(time.Now().UnixNano()))
 	older := NewPopulatedOldU(popr, true)
+	// need optional field to be always initialized, to check it's lost in this test
+	older.Field1 = code_google_com_p_gogoprotobuf_proto.String(randStringUnrecognized(popr))
 	data1, err := code_google_com_p_gogoprotobuf_proto.Marshal(older)
 	if err != nil {
 		panic(err)
@@ -143,12 +145,18 @@ func TestOldUToU(t *testing.T) {
 	}
 
 	older2 := &OldU{}
-	if err := code_google_com_p_gogoprotobuf_proto.Unmarshal(data2, newer); err != nil {
+	if err := code_google_com_p_gogoprotobuf_proto.Unmarshal(data2, older2); err != nil {
 		panic(err)
 	}
 
-	// check that 2 messages are NOT equal (since U has no XXX_unrecognized)
-	if err := older.VerboseEqual(older2); err == nil {
+	// check that Field1 is lost
+	if older2.Field1 != nil {
+		t.Fatalf("field must be lost, but it's not, older: %#v, older2: %#v", older, older2)
+	}
+
+	// now restore Field1 and messages should be equal now
+	older2.Field1 = older.Field1
+	if err := older.VerboseEqual(older2); err != nil {
 		t.Fatalf("%#v !VerboseProto %#v, since %v", older, older2, err)
 	}
 }
@@ -156,6 +164,8 @@ func TestOldUToU(t *testing.T) {
 func TestOldUnoM(t *testing.T) {
 	popr := math_rand.New(math_rand.NewSource(time.Now().UnixNano()))
 	older := NewPopulatedOldUnoM(popr, true)
+	// need optional field to be always initialized, to check it's lost in this test
+	older.Field1 = code_google_com_p_gogoprotobuf_proto.String(randStringUnrecognized(popr))
 	data1, err := code_google_com_p_gogoprotobuf_proto.Marshal(older)
 	if err != nil {
 		panic(err)
@@ -171,12 +181,18 @@ func TestOldUnoM(t *testing.T) {
 	}
 
 	older2 := &OldUnoM{}
-	if err := code_google_com_p_gogoprotobuf_proto.Unmarshal(data2, newer); err != nil {
+	if err := code_google_com_p_gogoprotobuf_proto.Unmarshal(data2, older2); err != nil {
 		panic(err)
 	}
 
-	// check that 2 messages are NOT equal (since U has no XXX_unrecognized)
-	if err := older.VerboseEqual(older2); err == nil {
+	// check that Field1 is lost
+	if older2.Field1 != nil {
+		t.Fatalf("field must be lost, but it's not, older: %#v, older2: %#v", older, older2)
+	}
+
+	// now restore Field1 and messages should be equal now
+	older2.Field1 = older.Field1
+	if err := older.VerboseEqual(older2); err != nil {
 		t.Fatalf("%#v !VerboseProto %#v, since %v", older, older2, err)
 	}
 }
