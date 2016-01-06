7b1331554dbe882cb3613ee8f1824a5583627963
Walter Schulze <awalterschulze@gmail.com>
2016-01-06 14:20:57 +0200

    try changing travis folder for protobuf

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -19,6 +19,8 @@ case "$PROTOBUF_VERSION" in
     ;;
 esac
 
+cd /home/travis
+
 wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz
 tar xzf $basename.tar.gz
 
