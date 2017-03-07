1b20eee63c440d75d647f0be13c2ce4873c70748
Johan Brandhorst <johan@cognitivelogic.com>
2017-03-07 16:28:22 +0000

    Compile time warning when both Sizer and Protosizer are used (#271)
    * Add generation time error to sizer plugin
    
    * Add test
    
    * Update timestamp in test

--- plugin/size/size.go
+++ plugin/size/size.go
@@ -121,6 +121,7 @@ package size
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
@@ -576,6 +577,10 @@ func (p *size) Generate(file *generator.FileDescriptor) {
 	}
 	for _, message := range file.Messages() {
 		sizeName := ""
+		if gogoproto.IsSizer(file.FileDescriptorProto, message.DescriptorProto) && gogoproto.IsProtoSizer(file.FileDescriptorProto, message.DescriptorProto) {
+			fmt.Fprintf(os.Stderr, "ERROR: message %v cannot support both sizer and protosizer plugins\n", generator.CamelCase(*message.Name))
+			os.Exit(1)
+		}
 		if gogoproto.IsSizer(file.FileDescriptorProto, message.DescriptorProto) {
 			sizeName = "Size"
 		} else if gogoproto.IsProtoSizer(file.FileDescriptorProto, message.DescriptorProto) {
