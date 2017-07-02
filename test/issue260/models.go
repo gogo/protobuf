13d11a77b68819df28432771fba772b51ef5a562
rogeralsing <rogeralsing@gmail.com>
2017-07-02 11:22:36 +0200

    fix broken test

--- test/issue260/models.go
+++ test/issue260/models.go
@@ -3,8 +3,8 @@ package issue260
 import "time"
 
 type Dropped struct {
-	Name string
-	Age  int32
+	Name string `protobuf:"bytes,1,opt,name=name,json=name"`
+	Age  int32  `protobuf:"varint,2,opt,name=age,json=age"`
 }
 
 func (d *Dropped) Drop() bool {
@@ -12,8 +12,8 @@ func (d *Dropped) Drop() bool {
 }
 
 type DroppedWithoutGetters struct {
-	Width             int64
-	Height            int64
+	Width             int64      `protobuf:"varint,1,opt,name=width,json=width"`
+	Height            int64      `protobuf:"varint,2,opt,name=height,json=height"`
 	Timestamp         time.Time  `protobuf:"bytes,3,opt,name=timestamp,stdtime" json:"timestamp"`
 	NullableTimestamp *time.Time `protobuf:"bytes,4,opt,name=nullable_timestamp,json=nullableTimestamp,stdtime" json:"nullable_timestamp,omitempty"`
 }
