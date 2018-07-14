bd19531613f8ac477f94a6ae5b7a79fc4716e138
Donald Graham <donald.graham@vastech.co.za>
2018-07-14 07:18:57 +0000

    dev: added tests for deterministic marshaling

--- test/deterministic/deterministic_test.go
+++ test/deterministic/deterministic_test.go
@@ -77,18 +77,134 @@ func TestOrderedMap(t *testing.T) {
 		t.Fatal(err)
 	}
 	if bytes.Compare(data1, data2) != 0 {
-		t.Fatal("byte arrays are not the same")
+		t.Fatal("byte arrays are not the same\n", data1, "\n", data2)
 	}
 }
 
 func TestUnorderedMap(t *testing.T) {
-	var b proto.Buffer
-	b.SetDeterministic(true)
 	m := getTestMap()
 	in := &UnorderedMap{
 		StringMap: m,
 	}
+	var b proto.Buffer
+	b.SetDeterministic(true)
 	if err := b.Marshal(in); err == nil {
-		t.Fatalf("Expected Marshal to fail, rejecting deterministic flag")
+		t.Fatalf("Expected Marshal to return error rejecting deterministic flag")
+	}
+}
+
+func TestMapNoMarshaler(t *testing.T) {
+	m := getTestMap()
+	in := &MapNoMarshaler{
+		StringMap: m,
+	}
+
+	var b1 proto.Buffer
+	b1.SetDeterministic(true)
+	if err := b1.Marshal(in); err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	data1 := b1.Bytes()
+
+	out := &MapNoMarshaler{}
+	err := proto.Unmarshal(data1, out)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := in.VerboseEqual(out); err != nil {
+		t.Fatal(err)
+	}
+
+	var b2 proto.Buffer
+	b2.SetDeterministic(true)
+	if err := b2.Marshal(in); err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	data2 := b2.Bytes()
+
+	if bytes.Compare(data1, data2) != 0 {
+		t.Fatal("byte arrays are not the same:\n", data1, "\n", data2)
+	}
+}
+
+func TestOrderedNestedMap(t *testing.T) {
+	var b proto.Buffer
+	in := &NestedOrderedMap{
+		StringMap: getTestMap(),
+		NestedMap: &NestedMap1{
+			NestedStringMap: getTestMap(),
+		},
+	}
+	if err := b.Marshal(in); err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	data1 := b.Bytes()
+	out := &NestedOrderedMap{}
+	err := proto.Unmarshal(data1, out)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := in.VerboseEqual(out); err != nil {
+		t.Fatal(err)
+	}
+	data2, err := proto.Marshal(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes.Compare(data1, data2) != 0 {
+		t.Fatal("byte arrays are not the same\n", data1, "\n", data2)
+	}
+}
+
+func TestUnorderedNestedMap(t *testing.T) {
+	in := &NestedUnorderedMap{
+		StringMap: getTestMap(),
+		NestedMap: &NestedMap2{
+			NestedStringMap: getTestMap(),
+		},
+	}
+	var b proto.Buffer
+	b.SetDeterministic(true)
+	if err := b.Marshal(in); err == nil {
+		t.Fatalf("Expected Marshal to return error rejecting deterministic flag")
+	}
+}
+
+func TestOrderedNestedStructMap(t *testing.T) {
+	var b proto.Buffer
+	m := getTestMap()
+	in := &NestedMap1{
+		NestedStringMap: m,
+	}
+	if err := b.Marshal(in); err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	data1 := b.Bytes()
+	out := &NestedMap1{}
+	err := proto.Unmarshal(data1, out)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := in.VerboseEqual(out); err != nil {
+		t.Fatal(err)
+	}
+	data2, err := proto.Marshal(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes.Compare(data1, data2) != 0 {
+		t.Fatal("byte arrays are not the same\n", data1, "\n", data2)
+	}
+}
+
+func TestUnorderedNestedStructMap(t *testing.T) {
+	m := getTestMap()
+	in := &NestedMap2{
+		NestedStringMap: m,
+	}
+	var b proto.Buffer
+	b.SetDeterministic(true)
+	if err := b.Marshal(in); err == nil {
+		t.Fatalf("Expected Marshal to return error rejecting deterministic flag")
 	}
 }
