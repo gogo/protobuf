65a9a066e7780192a17a4ac07150ad04c343dc5a
Donald Graham <donald.graham@vastech.co.za>
2018-08-08 13:09:43 +0000

    regenerated

--- protoc-gen-gogo/testdata/deprecated/deprecated.pb.go
+++ protoc-gen-gogo/testdata/deprecated/deprecated.pb.go
@@ -11,8 +11,10 @@ import proto "github.com/gogo/protobuf/proto"
 import fmt "fmt"
 import math "math"
 
-import context "golang.org/x/net/context"
-import grpc "google.golang.org/grpc"
+import (
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+)
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
@@ -134,7 +136,9 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// Client API for DeprecatedService service
+// DeprecatedServiceClient is the client API for DeprecatedService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 
 // Deprecated: Do not use.
 type DeprecatedServiceClient interface {
