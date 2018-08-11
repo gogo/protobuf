1f3c4d370d30ef09cba2788b1bf172df02cbf4b0
Peter Edge <peter.edge@gmail.com>
2018-08-11 20:38:05 +0200

    Add commented out code for gRPC unary server method

--- protoc-gen-gogo/grpc/grpc.go
+++ protoc-gen-gogo/grpc/grpc.go
@@ -408,7 +408,17 @@ func (g *grpc) generateServerMethod(servName, fullServName string, method *pb.Me
 
 	if !method.GetServerStreaming() && !method.GetClientStreaming() {
 		g.P("func ", hname, "(srv interface{}, ctx ", contextPkg, ".Context, dec func(interface{}) error, interceptor ", grpcPkg, ".UnaryServerInterceptor) (interface{}, error) {")
+
+		//inDesc := g.gen.ObjectNamed(method.GetInputType())
+		//inPkgname := g.gen.DefaultPackageName(inDesc)
+		//inBasename := generator.CamelCaseSlice(inDesc.TypeName())
+		//if gogoproto.HasPool(inDesc.File().FileDescriptorProto) {
+		//g.P(`in := `, inPkgname, `Get`, inBasename, `()`)
+		//g.P(`defer in.Recycle()`)
+		//} else {
 		g.P("in := new(", inType, ")")
+		//}
+
 		g.P("if err := dec(in); err != nil { return nil, err }")
 		g.P("if interceptor == nil { return srv.(", servName, "Server).", methName, "(ctx, in) }")
 		g.P("info := &", grpcPkg, ".UnaryServerInfo{")
