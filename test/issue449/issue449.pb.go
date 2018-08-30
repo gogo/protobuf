699a6c6d762edcd567e2c4e385bcf743e6af49d6
donaldgraham <donald.w.graham@gmail.com>
2018-08-30 15:43:13 +0000

    regenerated after nil check

--- test/issue449/issue449.pb.go
+++ test/issue449/issue449.pb.go
@@ -307,6 +307,9 @@ func encodeVarintIssue449(dAtA []byte, offset int, v uint64) int {
 	return offset + 1
 }
 func (m *CodeGenMsg) Size() (n int) {
+	if m == nil {
+		return 0
+	}
 	var l int
 	_ = l
 	if m.Int64ReqPtr != nil {
