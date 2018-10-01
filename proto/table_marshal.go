ad33d880fa4ca447b156815f58e0f52688c95af2
Jacques Marais <jacquesmaraisrsa@gmail.com>
2018-10-01 05:43:26 +0000

    merged in golang/protobuf commit 1325a051a2753cd67556b182843b1b693d0854cd -  proto: fix quadratic behavior in nested map marshaling (no size caching for stdduration/stdtime)

--- proto/table_marshal.go
+++ proto/table_marshal.go
@@ -2373,15 +2373,18 @@ func makeMapMarshaler(f *reflect.StructField) (sizer, marshaler) {
 	tags := strings.Split(f.Tag.Get("protobuf"), ",")
 	keyTags := strings.Split(f.Tag.Get("protobuf_key"), ",")
 	valTags := strings.Split(f.Tag.Get("protobuf_val"), ",")
+	stdOptions := false
 	for _, t := range tags {
 		if strings.HasPrefix(t, "customtype=") {
 			valTags = append(valTags, t)
 		}
 		if t == "stdtime" {
 			valTags = append(valTags, t)
+			stdOptions = true
 		}
 		if t == "stdduration" {
 			valTags = append(valTags, t)
+			stdOptions = true
 		}
 	}
 	keySizer, keyMarshaler := typeMarshaler(keyType, keyTags, false, false) // don't omit zero value in map
@@ -2395,6 +2398,25 @@ func makeMapMarshaler(f *reflect.StructField) (sizer, marshaler) {
 	// value.
 	// Key cannot be pointer-typed.
 	valIsPtr := valType.Kind() == reflect.Ptr
+
+	// If value is a message with nested maps, calling
+	// valSizer in marshal may be quadratic. We should use
+	// cached version in marshal (but not in size).
+	// If value is not message type, we don't have size cache,
+	// but it cannot be nested either. Just use valSizer.
+	valCachedSizer := valSizer
+	if valIsPtr && !stdOptions && valType.Elem().Kind() == reflect.Struct {
+		u := getMarshalInfo(valType.Elem())
+		valCachedSizer = func(ptr pointer, tagsize int) int {
+			// Same as message sizer, but use cache.
+			p := ptr.getPointer()
+			if p.isNil() {
+				return 0
+			}
+			siz := u.cachedsize(p)
+			return siz + SizeVarint(uint64(siz)) + tagsize
+		}
+	}
 	return func(ptr pointer, tagsize int) int {
 			m := ptr.asPointerTo(t).Elem() // the map
 			n := 0
@@ -2421,7 +2443,7 @@ func makeMapMarshaler(f *reflect.StructField) (sizer, marshaler) {
 				kaddr := toAddrPointer(&ki, false)    // pointer to key
 				vaddr := toAddrPointer(&vi, valIsPtr) // pointer to value
 				b = appendVarint(b, tag)
-				siz := keySizer(kaddr, 1) + valSizer(vaddr, 1) // tag of key = 1 (size=1), tag of val = 2 (size=1)
+				siz := keySizer(kaddr, 1) + valCachedSizer(vaddr, 1) // tag of key = 1 (size=1), tag of val = 2 (size=1)
 				b = appendVarint(b, uint64(siz))
 				b, err = keyMarshaler(b, kaddr, keyWireTag, deterministic)
 				if err != nil {
