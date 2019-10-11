5be52d540c806dc96b9c5182a3f50b7b086ade37
Jacques Marais <jacquesmaraisrsa@gmail.com>
2019-10-11 12:50:34 +0000

    umarshal - refactor skip from recursive calls to a loop.

--- test/oneof/combos/unmarshaler/one.pb.go
+++ test/oneof/combos/unmarshaler/one.pb.go
@@ -5891,6 +5891,7 @@ func (m *CustomOneof) Unmarshal(dAtA []byte) error {
 func skipOne(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
+	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -5922,10 +5923,8 @@ func skipOne(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
-			return iNdEx, nil
 		case 1:
 			iNdEx += 8
-			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -5946,55 +5945,30 @@ func skipOne(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthOne
 			}
 			iNdEx += length
-			if iNdEx < 0 {
-				return 0, ErrInvalidLengthOne
-			}
-			return iNdEx, nil
 		case 3:
-			for {
-				var innerWire uint64
-				var start int = iNdEx
-				for shift := uint(0); ; shift += 7 {
-					if shift >= 64 {
-						return 0, ErrIntOverflowOne
-					}
-					if iNdEx >= l {
-						return 0, io.ErrUnexpectedEOF
-					}
-					b := dAtA[iNdEx]
-					iNdEx++
-					innerWire |= (uint64(b) & 0x7F) << shift
-					if b < 0x80 {
-						break
-					}
-				}
-				innerWireType := int(innerWire & 0x7)
-				if innerWireType == 4 {
-					break
-				}
-				next, err := skipOne(dAtA[start:])
-				if err != nil {
-					return 0, err
-				}
-				iNdEx = start + next
-				if iNdEx < 0 {
-					return 0, ErrInvalidLengthOne
-				}
-			}
-			return iNdEx, nil
+			depth++
 		case 4:
-			return iNdEx, nil
+			if depth == 0 {
+				return 0, ErrUnexpectedEndOfGroupOne
+			}
+			depth--
 		case 5:
 			iNdEx += 4
-			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
+		if iNdEx < 0 {
+			return 0, ErrInvalidLengthOne
+		}
+		if depth == 0 {
+			return iNdEx, nil
+		}
 	}
-	panic("unreachable")
+	return 0, io.ErrUnexpectedEOF
 }
 
 var (
-	ErrInvalidLengthOne = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowOne   = fmt.Errorf("proto: integer overflow")
+	ErrInvalidLengthOne        = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowOne          = fmt.Errorf("proto: integer overflow")
+	ErrUnexpectedEndOfGroupOne = fmt.Errorf("proto: unexpected end of group")
 )
